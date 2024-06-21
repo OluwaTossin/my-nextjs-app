0e65951423c5e86379b75e2d5394a045be72fac4
OluwaTossin <temitopep450@gmail.com>
2024-06-21 17:46:52 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+# variables.tf
+
+variable "region" {
+  description = "The AWS region to create resources in"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
